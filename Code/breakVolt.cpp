de735cbe8f858f9f139f95d14ba21c2fc2ebecb3
Jeremie Cote <jeremie.w.cote@gmail.com>
2020-09-20 20:18:45 -0400

    Created header file, moved common functions to it

--- Code/breakVolt.cpp
+++ Code/breakVolt.cpp
@@ -1,53 +1,8 @@
 // @(#)root
 
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <vector>
 #include <stdlib>
 #include <TFile.h>
-
-using namespace std;
-
-vector< vector<double> > getData(string file, char delim, int start) {
-  string fileName = file;
-  char delimiter = delim;
-  int startLine = start;
-
-  ifstream ifs(fileName.c_str());
-
-  vector< vector<double> > data;
-  vector<double> row;
-  string line;
-  string entry;
-
-  int linecount = 0;
-  while( getline(ifs, line) ) {
-    linecount ++;
-    row.clear();
-    stringstream ss(line);
-    while(getline(ss, entry, delimiter)){
-      row.push_back( strtod(entry.c_str(), NULL) );
-    }
-    if (linecount >= startLine) {
-      data.push_back(row);
-    }
-  }
-  return data;
-}
-
-double pythagoreanTheorem(vector<double> numbers) {
-  vector<double> num = numbers;
-
-  double sum = 0;
-  for(int i = 0; i < num.size(), i++) {
-    sum += pow(num[i], 2);
-  }
-  sum = pow(sum, 0.5);
-
-  return sum;
-}
+#include "SiPM.h"
 
 void breakVolt() {
   string fileName = "../Data/Vbr.csv";
--- Code/gainFit.cpp
+++ Code/gainFit.cpp
@@ -1,46 +1,14 @@
 // @(#)root
 
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <vector>
 #include <stdlib>
 #include <TFile.h>
-
-using namespace std;
-
-vector< vector<double> > getData(string file, char delim) {
-  string fileName = file;
-  char delimiter = delim;
-  cout<<fileName<<endl;
-  ifstream ifs(fileName.c_str());
-
-
-  vector< vector<double> > data;
-  vector<double> row;
-  string line;
-  string entry;
-
-  int linecount = 0;
-  while(getline(ifs, line))
-  {
-    linecount ++;
-    row.clear();
-    stringstream ss(line);
-    while(getline(ss, entry, delimiter)){
-      row.push_back( strtod(entry.c_str(), NULL) );
-    }
-    data.push_back(row);
-  }
-  return data;
-}
+#include "SiPM.h"
 
 void gainFit() {
   string fileName = "../Data/100att_gain32.csv";
   string fileName = "C:/Users/jerem/Documents/School/Uni/Courses/4th_Year/PHYS4007/SiPM/Data/100att_gain32.csv";
 
-  vector< vector<double> > data = getData(fileName, ',');
+  vector< vector<double> > data = getData(fileName, ',', 0);
 
   TCanvas *c1 = new TCanvas();
 
@@ -62,7 +30,7 @@ void gainFit() {
   gr->SetTitle("Gain Plot");
   gr->GetXaxis()->SetTitle("Detected");
   gr->GetYaxis()->SetTitle("Channel");
-  gr->Draw("ACP");
+  gr->Draw("AP");
 
   c1->SaveAs("C:/Users/jerem/Documents/School/Uni/Courses/4th_Year/PHYS4007/SiPM/Report/Figures/gainFit.png");
 }
